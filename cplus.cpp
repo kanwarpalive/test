3c1752d2fdfc6ab2bd4813e58aecc1890cfc07e8
kanwarpalmehra <kanwarpalive@gmail.com>
2017-11-26 01:15:34 -0800

    cplus add

---
+++ cplus.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main(){
+	
+	cout<<"************************"<<endl;
+
+	int kon[] = {23,3,46,21,40,6};
+	int lez = sizeof(kon)/4;
+	
+	cout<<"size of array: "<<lez;
+
+	for(int i=0;i>lez;i++){
+		cout<<kon[i];
+	}
+
+	cout<<endl<<"************************"<<endl<<endl;
+
+
+	return 0;
+}
+
+	
